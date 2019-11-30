148634c3bf567f984c96024e39bedad105205078
Ashish Deora <ashish.deora@greatplacetowork.com>
2019-11-30 17:27:59 +0530

    End of Section 4

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private authservice: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authservice.register(this.model).subscribe(
+      (response) => {
+        console.log('Registered');
+      },
+      (error) => {
+        console.log('Something horribly went wrong');
+      }
+    );
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('Cancelled');
+  }
+}
--- DatingApp.api/Controllers/ValuesController.cs
+++ DatingApp.api/Controllers/ValuesController.cs
@@ -23,6 +23,7 @@ namespace DatingApp.API.Controllers
 
         
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> GetValues()
         {
             var values = await _context.Values.ToListAsync();
